d4129897cfa7e437203b6f7d637a7cc0c5ada388
JerryWang0122 <school832@gmail.com>
2024-05-10 11:15:36 +0800

    feat: 1115

---
+++ src/main/java/mvc/user/model/po/BaseDate.java
@@ -0,0 +1,13 @@
+package mvc.user.model.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseDate {
+    private Integer itemId;
+    private String itemName;
+}
