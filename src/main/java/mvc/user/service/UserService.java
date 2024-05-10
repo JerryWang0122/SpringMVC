f466d0c64f2c20b4c17d45b4f419d59d4a91b1bc
JerryWang0122 <school832@gmail.com>
2024-05-10 14:08:09 +0800

    feat: user service

--- src/main/java/mvc/user/service/UserService.java
+++ src/main/java/mvc/user/service/UserService.java
@@ -1,4 +1,35 @@
 package mvc.user.service;
 
+import mvc.user.dao.UserDao;
+import mvc.user.model.po.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public List<User> findUsers() {
+        return userDao.findAllUsers();
+    }
+
+    public User getUser(Integer userId) {
+        return userDao.getUserById(userId);
+    }
+
+    public Boolean addUser(User user) {
+        return userDao.addUser(user) > 0;
+    }
+
+    public Boolean updateUser(Integer userId, User user) {
+        return userDao.updateUser(userId, user) > 0;
+    }
+
+    public Boolean deleteUser(Integer userId) {
+        return userDao.deleteUser(userId) > 0;
+    }
 }
