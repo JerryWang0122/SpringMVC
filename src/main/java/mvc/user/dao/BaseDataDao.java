d9d14e164b4e96a920a4fad25b2d48394b75f9bd
JerryWang0122 <school832@gmail.com>
2024-05-10 11:30:35 +0800

    feat: dao interface

---
+++ src/main/java/mvc/user/dao/BaseDataDao.java
@@ -0,0 +1,18 @@
+package mvc.user.dao;
+
+import mvc.user.model.po.Education;
+import mvc.user.model.po.Gender;
+import mvc.user.model.po.Interest;
+
+import java.util.List;
+
+public interface BaseDataDao {
+    List<Education> findAllEducations();
+    Education getEducationById(Integer id);
+
+    List<Gender> findAllGenders();
+    Gender getGenderById(Integer id);
+
+    List<Interest> findAllInterests();
+    Interest getInterestById(Integer id);
+}
