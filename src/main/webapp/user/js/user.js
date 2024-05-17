3e6056d5da43c12b8993912107c2131ba83e7580
JerryWang0122 <school832@gmail.com>
2024-05-17 10:00:21 +0800

    feat: 5/17上課開始

--- src/main/webapp/user/js/user.js
+++ src/main/webapp/user/js/user.js
@@ -1,4 +1,6 @@
-
+/*********************************************************************************************************
+ * 共用函式
+ *********************************************************************************************************/
 const $ = (id) => document.getElementById(id);
 
 // 定義一個非同步函數來加載 html 內容
@@ -13,6 +15,17 @@ const loadHTML = async (url, containerId) => {
     }
 };
 
+const handleEvent = async(event, className, callback) => {
+    if (!event.target.classList.contains(className)) {
+        return;
+    }
+    const id = event.target.getAttribute('data-id');
+    callback(id);
+}
+
+/*********************************************************************************************************
+ * 資料渲染
+ *********************************************************************************************************/
 // 渲染 User 資料的配置
 const renderUser = ({ id, name, gender, age, birth, education, interestNames, interests, resume }) => `
 <tr>
@@ -39,7 +52,7 @@ const fetchAndRenderData = async(url, containerId, renderFn) => {
     try {
         const response = await fetch(fullUrl);  // 等待 fetch 請求完成
         const { state, message, data } = await response.json();     // 等待回應本文內容
-        console.log(state, message, data)
+        // console.log(state, message, data)
 
         // data 是一個陣列
         // console.log(renderFn(data[0]))
@@ -59,10 +72,37 @@ const fetchAndRenderData = async(url, containerId, renderFn) => {
     }
 };
 
-const handleUpdateUser = (id) => {
-    console.log('按下修改' + id)
+
+// 加載表單選項(學歷、興趣)
+const loadFormOptions = async() => {
+    // 加載學歷選項
+    const educationOptions = await fetch('http://localhost:8080/SpringMVC_war_exploded/mvc/rest/user/educations');
+    var { state, message, data } = await educationOptions.json();
+    // console.log(data);
+    data.forEach(education => {
+        const opt = document.createElement('option');
+        opt.value = education.id;
+        opt.textContent = education.name;
+        $('educationId').appendChild(opt);
+    })
+
+    // 加載興趣選項
+    const interestOptions = await fetch('http://localhost:8080/SpringMVC_war_exploded/mvc/rest/user/interests');
+    var { state, message, data } = await interestOptions.json();
+    // console.log(data);
+    data.forEach(interest => {
+        const opt = document.createElement('option');
+        opt.value = interest.id;
+        opt.textContent = interest.name;
+        $('interestIds').appendChild(opt);
+    })
+
 };
 
+
+/*********************************************************************************************************
+ * User CRUD 操作
+ *********************************************************************************************************/
 const handleDeleteUser = async (id) => {
     console.log('按下刪除' + id);
     // 確認是否要刪除 ?
@@ -101,40 +141,6 @@ const handleDeleteUser = async (id) => {
     fetchAndRenderData('/mvc/rest/user', 'user-list-body', renderUser);
 };
 
-const handleEvent = async(event, className, callback) => {
-    if (!event.target.classList.contains(className)) {
-        return;
-    }
-    const id = event.target.getAttribute('data-id');
-    callback(id);
-}
-
-// 加載表單選項(學歷、興趣)
-const loadFormOptions = async() => {
-    // 加載學歷選項
-    const educationOptions = await fetch('http://localhost:8080/SpringMVC_war_exploded/mvc/rest/user/educations');
-    var { state, message, data } = await educationOptions.json();
-    // console.log(data);
-    data.forEach(education => {
-        const opt = document.createElement('option');
-        opt.value = education.id;
-        opt.textContent = education.name;
-        $('educationId').appendChild(opt);
-    })
-
-    // 加載興趣選項
-    const interestOptions = await fetch('http://localhost:8080/SpringMVC_war_exploded/mvc/rest/user/interests');
-    var { state, message, data } = await interestOptions.json();
-    console.log(data);
-    data.forEach(interest => {
-        const opt = document.createElement('option');
-        opt.value = interest.id;
-        opt.textContent = interest.name;
-        $('interestIds').appendChild(opt);
-    })
-
-};
-
 // 表單提交事件處理
 const handleFormSubmit = async(event) => {
     event.preventDefault();  // 停止表單的預設傳送行為
@@ -151,6 +157,20 @@ const handleFormSubmit = async(event) => {
         resume: $('resume').value
     };
 
+    const id = $('user-form').getAttribute('data-id');
+    const submitButtonText = $('form-submit-button').textContent;
+
+    if (submitButtonText === '新增') {
+        // 新增使用者
+        addUser(formData);
+    } else if (submitButtonText === '修改') {
+        // 更新使用者
+        updateUser(id, formData);
+    }
+};
+
+// 新增使用者
+const addUser = async(formData) => {
     const response = await fetch('http://localhost:8080/SpringMVC_war_exploded/mvc/rest/user', {
         method: 'POST',
         headers: {  // 一定要加
@@ -162,7 +182,80 @@ const handleFormSubmit = async(event) => {
     const { state, message, data } = await response.json();
     console.log(message);
     fetchAndRenderData('/mvc/rest/user', 'user-list-body', renderUser);
+    // 清除表單
+    clearForm();
 }
+
+// 取得並顯示使用者資料到表單上
+const getUserData = async(id) => {
+    const fullUrl = `http://localhost:8080/SpringMVC/mvc/rest/user/${id}`;
+    try {
+        const response = await fetch(fullUrl);
+        const {state, message, data} = await response.json();
+        const user = data;
+        console.log(user);
+        $('name').value = user.name;
+        $('age').value = user.age;
+        $('birth').value = user.birth;
+        $('educationId').value = user.educationId;
+        document.querySelector(`input[name="genderId"][value="${user.genderId}"]`).checked = true;
+
+        // 設置興趣多選框的選中狀態
+        const interestSelect = $('interestIds');
+        Array.from(interestSelect.options).forEach(option => {
+            option.selected = user.interestIds.includes(parseInt(option.value));
+        });
+
+        $('resume').value = user.resume;
+
+        $('user-form').setAttribute('data-id', user.id);
+        $('form-submit-button').textContent = '修改';
+    } catch(e) {
+        console.error(e);
+    }
+};
+
+// 更新使用者資料
+const updateUser = async(id, formData) => {
+    const fullUrl = `http://localhost:8080/SpringMVC/mvc/rest/user/${id}`;
+    try {
+        const response = await fetch(fullUrl, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+        const {state, message, data} = await response.json();
+        console.log(message);
+
+        // 重新資料渲染(fetch取資料+渲染)
+        fetchAndRenderData('/mvc/rest/user', 'user-list-body', renderUser);
+        // 清除表單
+        clearForm();
+    } catch(e) {
+        console.error(e);
+    }
+};
+
+// 按下修改鍵
+const handleUpdateUser = (id) => {
+    console.log('按下修改:' + id);
+    getUserData(id);
+};
+
+// 表單重製/清除表單
+const clearForm = () => {
+    $('name').value = '';
+    $('age').value = '';
+    $('birth').value = '';
+    $('educationId').value = '';
+    document.querySelector('input[name="genderId"]:checked').checked = false;
+    Array.from($('interestIds').options).forEach(option => option.selected = false);
+    $('resume').value = '';
+    $('user-form').removeAttribute('data-id');
+    $('form-submit-button').textContent = '新增';
+};
 
 // 待 DOM 加載完成之後再執行
 document.addEventListener("DOMContentLoaded", async() => {
